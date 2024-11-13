ab31d5fbb109e4b881cc03f188120dc34cb5bce1
Abhishek Sharma <153059659+abhisheksharma-github@users.noreply.github.com>
2024-11-13 11:52:18 +0000

    Baisc Question

--- Python-core/Nested.py
+++ Python-core/Nested.py
@@ -1,22 +1,27 @@
-# Taking three numbers as input 
+# Taking three numbers as input
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 num3 = int(input("Enter the third number: "))
 
-print("First Number is :",num1)
-print("Second Number is :",num2)
-print("Third Number:is ",num3)
-# Finding the greatest number using nested if-else Condition
-if num1 >= num2:
-    if num1 >= num3:
-        greatest = num1
-    else:
-        greatest = num3
-else:
-    if num2 >= num3:
-        greatest = num2
-    else:
-        greatest = num3
+print("First Number is:", num1)
+print("Second Number is:", num2)
+print("Third Number is:", num3)
 
-#result
-print("Greatest number is:", greatest)
+# Checking if all numbers are equal
+if num1 == num2 == num3:
+    print("All three numbers are equal.")
+else:
+    # Finding the greatest number using nested if-else Condition
+    if num1 >= num2:
+        if num1 >= num3:
+            greatest = num1
+        else:
+            greatest = num3
+    else:
+        if num2 >= num3:
+            greatest = num2
+        else:
+            greatest = num3
+
+    # Result
+    print("Greatest number is:", greatest)
