a9db3d652f8cf8605acbcf1084e6817da867e28f
Abhishek Sharma <153059659+abhisheksharma-github@users.noreply.github.com>
2024-11-13 11:07:48 +0000

    Simple Question

--- Python-core/Question1.py
+++ Python-core/Question1.py
@@ -1,10 +1,10 @@
 # Input the three sides
-a = float(input("Enter side A: "))
-b = float(input("Enter side B: "))
-c = float(input("Enter side C: "))
+side_a = float(input("Enter side A: "))
+side_b = float(input("Enter side B: "))
+side_c = float(input("Enter side C: "))
 
 # Check if they can form a triangle
-if a + b > c and a + c > b and b + c > a:
+if side_a + side_b > side_c and side_a + side_c > side_b and side_b + side_c > side_a:
     print("These sides can form a triangle.")
 else:
     print("These sides cannot form a triangle.")
